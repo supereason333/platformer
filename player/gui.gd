5d77fd1a2405a4d74c9214006bc89ce6c889af89
supereason333 <supereason333@gmail.com>
2024-04-15 21:34:19 +1200

    player kill and damage system kinda works also reset and respawn systems kinda work

--- player/gui.gd
+++ player/gui.gd
@@ -10,8 +10,8 @@ func _ready():
 	update_gui()
 
 
-func _on_player_health_updated(health):
+func _on_player_health_updated(_health):
 	update_gui()
 
-func _on_player_money_updated(money):
+func _on_player_money_updated(_money):
 	update_gui()
