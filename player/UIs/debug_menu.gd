4a0309a3fc7bbc01a9d21b7d0b6de5111a6a0066
supereason333 <supereason333@gmail.com>
2024-04-18 14:44:36 +1200

    added a button to debug menu

--- player/UIs/debug_menu.gd
+++ player/UIs/debug_menu.gd
@@ -4,15 +4,19 @@ signal debug_player_set_health(value)
 
 var player = get_parent()
 var health_spin_box = 0
+var dragging = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_just_pressed("debug_menu_toggle_key"):
 		self.visible = !self.visible
+	if dragging:
+		# make it move with the mouse
+		print_debug(get_viewport().get_mouse_position())
 
 func _on_spin_box_value_changed(value):
 	GlobalPlayer.player_jumps = int(value)
@@ -31,3 +35,9 @@ func _on_kill_pressed():
 
 func _on_set_max_health_pressed():
 	GlobalPlayer.player_max_health = health_spin_box
+
+func _on_button_button_down():
+	dragging = true
+
+func _on_button_button_up():
+	dragging = false
